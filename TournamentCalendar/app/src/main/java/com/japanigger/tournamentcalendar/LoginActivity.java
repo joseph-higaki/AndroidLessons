c21c74b9c6c7fabb38b737f6260c513e4f3fc2c5
joseph-higaki <josephhigaki@hotmail.com>
2015-05-21 20:14:33 -0500

    bypass facebook login

--- TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/LoginActivity.java
+++ TournamentCalendar/app/src/main/java/com/japanigger/tournamentcalendar/LoginActivity.java
@@ -1,24 +1,63 @@
 package com.japanigger.tournamentcalendar;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
 
-public class LoginActivity extends Activity {
+import roboguice.activity.RoboActivity;
+import roboguice.inject.ContentView;
 
+@ContentView(R.layout.activity_login)
+public class LoginActivity extends RoboActivity {
+    private CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FacebookSdk.sdkInitialize(getApplicationContext());
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+
+        callbackManager = CallbackManager.Factory.create();
+        LoginManager.getInstance().registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("FacebookApp", "onSuccess");
+                loginSuccess();
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d("FacebookApp", "onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Log.d("FacebookApp", "onError");
+
+            }
+        });
+    }
+
+    private void loginSuccess(){
+        Intent intent = new Intent(this, TeamPlayerListActivity.class);
+        startActivity(intent);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_login, menu);
+        //getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
 
@@ -35,5 +74,23 @@ public class LoginActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // Logs 'install' and 'app activate' App Events.
+        AppEventsLogger.activateApp(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        // Logs 'app deactivate' App Event.
+        AppEventsLogger.deactivateApp(this);
+    }
+
+    public void pretendFacebookLoginWorks(View view) {
+        loginSuccess();
     }
 }
